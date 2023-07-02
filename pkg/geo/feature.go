d090606c383075038a5ad2e21f475f43c31ac22a
Maksim Korenev <maksim.korenev@covergo.com>
2023-07-03 00:35:06 +0800

    03/jul/2023

--- pkg/geo/feature.go
+++ pkg/geo/feature.go
@@ -1,26 +1,24 @@
 package geo
 
 import (
-	"encoding/json"
-	"github.com/mitchellh/mapstructure"
 	"github.com/qwibi/qwibi-go-sdk/pkg/geometry"
 	"github.com/qwibi/qwibi-go-sdk/pkg/qlog"
 	"github.com/qwibi/qwibi-go-sdk/proto"
 )
 
-type QFeature struct {
-	Geometry   geometry.QGeometry `json:"geometry"`
-	Properties []byte             `json:"properties"`
+type QFeature[T geometry.QGeometry] struct {
+	Geometry   T      `json:"geometry"`
+	Properties []byte `json:"properties"`
 }
 
-func NewFeature(geometry geometry.QGeometry) *QFeature {
-	return &QFeature{
+func NewFeature[T geometry.QGeometry](geometry T) *QFeature[T] {
+	return &QFeature[T]{
 		Geometry:   geometry,
 		Properties: []byte{},
 	}
 }
 
-func NewFeaturePb(in *proto.QPBxFeature) (*QFeature, error) {
+func NewFeaturePb(in *proto.QPBxFeature) (*QFeature[geometry.QGeometry], error) {
 	geometry, err := geometry.NewGeometryPb(in.Geometry)
 	if err != nil {
 		return nil, qlog.Error(err)
@@ -30,47 +28,47 @@ func NewFeaturePb(in *proto.QPBxFeature) (*QFeature, error) {
 	return feature, nil
 }
 
-func (c *QFeature) Pb() *proto.QPBxFeature {
+func (c *QFeature[T]) Pb() *proto.QPBxFeature {
 	return &proto.QPBxFeature{
 		Geometry:   c.Geometry.Pb(),
 		Properties: c.Properties,
 	}
 }
 
-func (f *QFeature) UnmarshalJSON(data []byte) error {
-	qlog.Infof("!!!!! data: %+s", data)
-
-	type Alias QFeature
-	aux := &struct {
-		Geometry map[string]interface{} `json:"geometry"`
-		*Alias
-	}{
-		Alias: (*Alias)(f),
-	}
-
-	if err := json.Unmarshal(data, &aux); err != nil {
-		return err
-	}
-
-	qlog.Infof("??? aux => %+v", aux)
-	qlog.Infof("??? geometry => %+v", aux.Geometry)
-	qlog.Infof("??? alias => %+v", aux.Alias)
-
-	if aux.Geometry == nil {
-		return qlog.Error("Error decoding Geometry property type nil")
-	}
-
-	switch v := aux.Geometry["type"]; v {
-	case geometry.QPointGeometryType:
-		var point geometry.QPoint
-		err := mapstructure.Decode(aux.Geometry, &point)
-		if err != nil {
-			return qlog.Error(err)
-		}
-		f.Geometry = point
-	default:
-		return qlog.Errorf("Unsupported geometry type: %s", v)
-	}
-
-	return nil
-}
+//func (f *QFeature[T]) UnmarshalJSON(data []byte) error {
+//	qlog.Infof("!!!!! data: %+s", data)
+//
+//	type Alias QFeature[T]
+//	aux := &struct {
+//		Geometry map[string]interface{} `json:"geometry"`
+//		*Alias
+//	}{
+//		Alias: (*Alias)(f),
+//	}
+//
+//	if err := json.Unmarshal(data, &aux); err != nil {
+//		return err
+//	}
+//
+//	qlog.Infof("??? aux => %+v", aux)
+//	qlog.Infof("??? geometry => %+v", aux.Geometry)
+//	qlog.Infof("??? alias => %+v", aux.Alias)
+//
+//	if aux.Geometry == nil {
+//		return qlog.Error("Error decoding Geometry property type nil")
+//	}
+//
+//	switch v := aux.Geometry["type"]; v {
+//	case geometry.QPointGeometryType:
+//		var point geometry.QPoint
+//		err := mapstructure.Decode(aux.Geometry, &point)
+//		if err != nil {
+//			return qlog.Error(err)
+//		}
+//		f.Geometry = point
+//	default:
+//		return qlog.Errorf("Unsupported geometry type: %s", v)
+//	}
+//
+//	return nil
+//}
